4603d6630e5528680a9183d09f9c898c0df9c08a
aguschin <1aguschin@gmail.com>
2020-11-26 17:33:10 +0300

    add pipenv, add test for pytest

---
+++ docker-flask-app/tests/test_player.py
@@ -0,0 +1,9 @@
+def test_player_explain():
+    from app import player
+    words = player.explain(word="test", n_words=5)
+    print(words)
+    
+def test_player_guess():
+    from app import player
+    words = player.guess(words=["taste", "cake", "sweet"], n_words=5)
+    print(words)

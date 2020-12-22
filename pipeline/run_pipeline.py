f11b78ce0753f4adf44ddfc6b26cf8bdfeeeec89
aguschin <1aguschin@gmail.com>
2020-12-22 12:47:32 +0300

    another file for training

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,5 +1,5 @@
 import fasttext
 
-file_path = 'misc.forsale.txt'
+file_path = 'sci.space.txt'
 model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3)
 model.save_model('skipgram.model')

306e45e677817bf801cfd623da8c345659fbc7b9
aguschin <1aguschin@gmail.com>
2020-12-22 13:55:30 +0300

    try fix

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,5 +1,5 @@
 import fasttext
 
 file_path = 'sci.space.txt'
-model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3)
+model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3, bucket=1000)
 model.save_model('skipgram.model')

179cb7649e4b0f2e5be6adaeff9c7662e998c417
aguschin <1aguschin@gmail.com>
2020-12-22 18:42:40 +0300

    fixes

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -8,9 +8,9 @@ remote_server_uri = "http://35.225.84.215"
 mlflow.set_tracking_uri(remote_server_uri)
 # Note: on Databricks, the experiment name passed to mlflow_set_experiment must be a
 # valid path in the workspace
-mlflow.set_experiment("/training-fasttext-embeddings")
+mlflow.set_experiment("training-fasttext-embeddings")
 with mlflow.start_run():
-    mlflow.log_param("HOST", os.environ['HOST'])
+    mlflow.log_param("USER", os.environ.get('HOME', '/home/None').split('/')[-1])
 
     file_path = 'sci.space.txt'
     model_path = 'skipgram.model'

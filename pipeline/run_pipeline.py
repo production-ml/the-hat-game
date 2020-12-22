b3a882eaf94360ab3b1f1ec5120fd76ed8eb3f94
aguschin <1aguschin@gmail.com>
2020-12-22 12:42:02 +0300

    change n_dim=3

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,5 +1,5 @@
 import fasttext
 
 file_path = 'misc.forsale.txt'
-model = fasttext.train_unsupervised(file_path, model='skipgram', dim=5)
+model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3)
 model.save_model('skipgram.model')

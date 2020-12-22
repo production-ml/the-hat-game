c5f8ddec2d5259b3733753e798d4f8906fee7a45
aguschin <1aguschin@gmail.com>
2020-12-22 18:32:45 +0300

    just powered up mlflow server on gke

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,5 +1,20 @@
-import fasttext
+import os
 
-file_path = 'sci.space.txt'
-model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3, bucket=1000)
-model.save_model('skipgram.model')
+import fasttext
+import mlflow
+
+
+remote_server_uri = "http://35.225.84.215"
+mlflow.set_tracking_uri(remote_server_uri)
+# Note: on Databricks, the experiment name passed to mlflow_set_experiment must be a
+# valid path in the workspace
+mlflow.set_experiment("/training-fasttext-embeddings")
+with mlflow.start_run():
+    mlflow.log_param("HOST", os.environ['HOST'])
+
+    file_path = 'sci.space.txt'
+    model_path = 'skipgram.model'
+    model = fasttext.train_unsupervised(file_path, model='skipgram', dim=3, bucket=1000)
+    model.save_model(model_path)
+
+    mlflow.log_artifact(model_path)
